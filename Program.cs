06e625bf438407d3df6be0627cf87dbd8217087e
vargaantonia <145546330+vargaantonia@users.noreply.github.com>
2025-02-10 12:30:01 +0100

    V3.0 - Majdnem kész, regisztráció nélkül

---
+++ Program.cs
@@ -0,0 +1,100 @@
+using IngatlanokBackend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using MySqlX.XDevAPI;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IngatlanokBackend
+{
+    public class Program
+    {
+        public static int SaltLength = 64;
+
+        public static Dictionary<string, Felhasznalok> LoggedInUsers = new Dictionary<string, Felhasznalok>();
+
+        public static string GenerateSalt()
+        {
+            Random random = new Random();
+            const string karakterek = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var salt = new StringBuilder();
+            for (int i = 0; i < SaltLength; i++)
+            {
+                salt.Append(karakterek[random.Next(karakterek.Length)]);
+            }
+            return salt.ToString();
+        }
+
+        public static string CreateSHA256(string input, string salt)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] data = sha256.ComputeHash(Encoding.UTF8.GetBytes(input + salt));
+                var sBuilder = new StringBuilder();
+                foreach (var b in data)
+                {
+                    sBuilder.Append(b.ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
+            builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            builder.Services.AddDbContext<IngatlanberlesiplatformContext>(options =>
+                options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin", builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000")
+                                 .AllowAnyMethod()        
+                                 .AllowAnyHeader();       
+                });
+            });
+
+            builder.Services.AddAuthentication("Bearer")
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"], 
+                        ValidAudience = builder.Configuration["Jwt:Audience"], 
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])) 
+                    };
+                });
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseCors("AllowSpecificOrigin");
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
+
+
+//Scaffold-DbContext "SERVER=localhost;PORT=3306;DATABASE=farm;USER=root;PASSWORD=;SSL MODE=none;" mysql.entityframeworkcore -outputdir Models -f
