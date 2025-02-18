3b95a31cda75efcf87c2d332200d9aa4fbf05884
vargaantonia <145546330+vargaantonia@users.noreply.github.com>
2025-02-18 17:45:27 +0100

    V4.2 - Foglalások (alap)

---
+++ DTOs/BookingRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace IngatlanokBackend.DTOs
+{
+    public class BookingRequestDTO
+    {
+            public int IngatlanId { get; set; }
+            public int BerloId { get; set; }
+            public DateTime KezdesDatum { get; set; }
+            public DateTime BefejezesDatum { get; set; }
+
+    }
+}
---
+++ DTOs/BookingResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace IngatlanokBackend.DTOs
+{
+    public class BookingResponseDTO
+    {
+        public int Id { get; set; }
+        public int IngatlanId { get; set; }
+        public int BerloId { get; set; }
+        public DateTime KezdesDatum { get; set; }
+        public DateTime BefejezesDatum { get; set; }
+        public string Allapot { get; set; }
+    }
+}
