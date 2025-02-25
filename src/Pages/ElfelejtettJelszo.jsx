1f5df26d3eab16bad634a34f1273dc67134ab3a4
vargaantonia <vargaa@kkszki.hu>
2025-02-25 16:20:03 +0100

    V4.11.3 - Elfelejtett jelszó

--- src/Pages/ElfelejtettJelszo.jsx
+++ src/Pages/ElfelejtettJelszo.jsx
@@ -1,34 +1,46 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import '../style.css';
 
 function PasswordReset() {
-    const [newPassword, setNewPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log('New password:', newPassword);
-};
-
-return (
-    <div className="Login">
-    <div className="wrapper">
-        <h1>Jelszó visszaállítása</h1>
-    <form onSubmit={handleSubmit}>
-        <div className="input-box">
-            <input
-                type="password"
-                placeholder="Új jelszó"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-                required
-            />
+        try {
+            const response = await axios.post(`https://localhost:7079/api/Felhasznalo/RequestPassword/${email}`);
+            setMessage(response.data);
+            setTimeout(() => {
+                navigate('/belepes');
+            }, 3000);
+        } catch (error) {
+            setMessage(error.response?.data || 'Hiba történt a kérés feldolgozása közben.');
+        }
+    };
+    
+    return (
+        <div className="Login">
+            <div className="wrapper">
+                <h1>Jelszó visszaállítása</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-box">
+                        <input
+                            type="email"
+                            placeholder="Email cím"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="btn">Jelszó visszaállítása</button>
+                </form>
+                {message && <p>{message}</p>}
+            </div>
         </div>
-        
-        <button type="submit" className="btn">Jelszó megváltoztatása</button>
-    </form>
-    </div>
-    </div>
-);
+    );
 }
 
 export default PasswordReset;
