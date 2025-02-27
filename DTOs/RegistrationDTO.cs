67047a652ca0554ca6af79d4d1a61253d515db0e
vargaantonia <145546330+vargaantonia@users.noreply.github.com>
2025-02-27 10:51:52 +0100

    V4.3.0 - Kisebb javítások

--- DTOs/RegistrationDTO.cs
+++ DTOs/RegistrationDTO.cs
@@ -6,6 +6,7 @@
         public string Email { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
-        public int PermissionId { get; set; } 
+        public int PermissionId { get; set; }
+        public string ProfilePicturePath { get; set; }
     }
 }
--- DTOs/userCreateDTO.cs
+++ DTOs/userCreateDTO.cs
@@ -6,7 +6,7 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public int? PermissionId { get; set; }
+        public int PermissionId { get; set; }
         public string? ProfilePicture { get; set; }
     }
 }
